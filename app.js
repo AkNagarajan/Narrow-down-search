0bbe6a8614698e2b7297d4fffd907767a9d58030
Akshaya Nagarajan <akshaya.n0811@gmail.com>
2017-02-23 18:30:21 +0530

    Replaced for loop filter with _filter function

--- app.js
+++ app.js
@@ -10,24 +10,12 @@
 			return $http.get("https://davids-restaurant.herokuapp.com/menu_items.json")
 			.then(function (response) 	
 			{
-				var foundItems = [{}];
 				var searchResult = 0;
 				var j = 0;
 				var menu = response.data.menu_items;
-				var searchStringInList ="";
-				var length = menu.length;
-console.log("Menu "+menu+" Length "+length);
-				for (var i=0; i<length; i++)
-				{
-				searchStringInList = menu.menu_items[i].description.toLowerCase();	
-				searchResult = searchTerm.indexOf(searchStringInList);								
-				if(searchResult >= 0)
-				{	//console.log("searchStringInList "+searchStringInList);
-					foundItems[j] = menu.menu_items[i];	
-					//console.log(menu.menu_items[i].name+" "+foundItems[j].id);
-					j=j+1;
-				}
-		        	}
+				var foundItems = _.filter (menu, function(item) {
+					return item.description.includes(searchTerm);
+				});
 				return foundItems;
 			});
 		}
@@ -37,7 +25,7 @@ console.log("Menu "+menu+" Length "+length);
 	function NarrowItDownController($scope,MenuSearchService){
 		$scope.narrowItDownMethod = function() {
 		var termToSearch = $scope.searchTerm;
-		MenuSearchService.getMatchedMenuItems(termToSearch);
+		$scope.foundItems = MenuSearchService.getMatchedMenuItems(termToSearch);
 		};
 	};
 	
